409b5c5ea70c40372b4e757379d79cc703088bda
aswanthasokankm <93219761+aswanthasokankm@users.noreply.github.com>
2021-10-27 00:33:12 +0530

    Create ssh.tf

---
+++ ssh.tf
@@ -0,0 +1,7 @@
+echo "# pub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aswanthasokankm/pub.git
+git push -u origin main
